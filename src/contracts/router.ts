87ee8943957469a7ce37a6dc5bfa879c8bc8776e
n1pu <nipudos-01@yahoo.com>
2023-09-20 00:41:39 +0200

    feat: add getSwapIn/getSwapOut

--- src/contracts/router.ts
+++ src/contracts/router.ts
@@ -1,7 +1,13 @@
-import { Client } from '@massalabs/massa-web3'
+import { Args, Client } from '@massalabs/massa-web3'
 import { LiquidityParameters, SwapParameters } from '../types'
 
-const U32_MAX = BigInt(2 ** 32 - 1)
+const U32_MAX = 2n ** 32n - 1n
+
+interface GetSwap {
+  pairAddress: string
+  amountIn: bigint
+  swapForY: boolean
+}
 
 export class IRouter {
   constructor(public address: string, private client: Client) {}
@@ -26,5 +32,51 @@ export class IRouter {
       fee: 100_000_000n,
       maxGas: U32_MAX
     })
+  }
+
+  async getSwapIn(
+    params: GetSwap
+  ): Promise<{ amountIn: bigint; feesIn: bigint }> {
+    return this.client
+      .smartContracts()
+      .readSmartContract({
+        targetAddress: this.address,
+        targetFunction: 'getSwapIn',
+        parameter: new Args()
+          .addString(params.pairAddress)
+          .addU64(params.amountIn)
+          .addBool(params.swapForY),
+        maxGas: U32_MAX
+      })
+      .then((result) => {
+        const args = new Args(result.returnValue)
+        return {
+          amountIn: args.nextU64(),
+          feesIn: args.nextU64()
+        }
+      })
+  }
+
+  async getSwapOut(
+    params: GetSwap
+  ): Promise<{ amountOut: bigint; feesIn: bigint }> {
+    return this.client
+      .smartContracts()
+      .readSmartContract({
+        targetAddress: this.address,
+        targetFunction: 'getSwapOut',
+        parameter: new Args()
+          .addString(params.pairAddress)
+          .addU64(params.amountIn)
+          .addBool(params.swapForY),
+        maxGas: U32_MAX
+      })
+      .then((result) => {
+        const args = new Args(result.returnValue)
+        return {
+          amountOut: args.nextU64(),
+          feesIn: args.nextU64()
+        }
+      })
   }
 }
