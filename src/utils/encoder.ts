ae6fb63bd8dfbdd52554e658e8a1bc87f9d0952b
n1pu <nipudos-01@yahoo.com>
2023-11-15 00:47:52 +0100

    fix: export decodeU256

--- src/utils/encoder.ts
+++ src/utils/encoder.ts
@@ -1,4 +1,6 @@
-export const strEncodeUTF16 = (str: string): Uint8Array => {
+import { bytesToU256 } from '@massalabs/massa-web3'
+
+const strEncodeUTF16 = (str: string): Uint8Array => {
   const buf = new ArrayBuffer(str.length * 2)
   const bufView = new Uint16Array(buf)
   for (let i = 0, strLen = str.length; i < strLen; i++) {
@@ -6,3 +8,6 @@ export const strEncodeUTF16 = (str: string): Uint8Array => {
   }
   return new Uint8Array(buf)
 }
+
+export const decodeU256 = (bytes: string): bigint =>
+  bytesToU256(strEncodeUTF16(bytes))
