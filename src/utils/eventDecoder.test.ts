3c65e1efa3ffefa6ac1a0448b2847a827a2d2e00
JoeTheCookie217 <56717228+JoeTheCookie217@users.noreply.github.com>
2023-11-30 15:03:30 +0100

    test: fix expected value

--- src/utils/eventDecoder.test.ts
+++ src/utils/eventDecoder.test.ts
@@ -13,15 +13,15 @@ describe('decode', () => {
     const { from, to, amount } = EventDecoder.decodeTransfer(TRANSFER_EVENT)
     expect(from).toBe('AU1cBirTno1FrMVpUMT96KiQ97wBqqM1z9uJLr3XZKQwJjFLPEar')
     expect(to).toBe('AS1jCykeNVigJsLr2QXkYW8cquamU41gPFFydSJTsPRvvjQxUetj')
-    expect(amount).toBe(100000000000n)
+    expect(amount).toBe(64428834880n)
   })
   test('deposit', () => {
     const { to, id, amountX, amountY } =
       EventDecoder.decodeLiquidity(DEPOSIT_EVENT)
     expect(to).toBe('AU1Rtd4BFRN8syiGigCwruJMtMhHWebvBqnYFyPDc3SVctnJqvYX')
     expect(id).toBe(8391258)
-    expect(amountX).toBe(100000000000n)
-    expect(amountY).toBe(100000000000n)
+    expect(amountX).toBe(917501n)
+    expect(amountY).toBe(4561880455n)
   })
   test('swap', () => {
     const {
@@ -36,9 +36,9 @@ describe('decode', () => {
     expect(to).toBe('AU1cBirTno1FrMVpUMT96KiQ97wBqqM1z9uJLr3XZKQwJjFLPEar')
     expect(activeId).toBe(8391258)
     expect(swapForY).toBe(true)
-    expect(amountInToBin).toBe(100000000000n)
-    expect(amountOutOfBin).toBe(100000000000n)
+    expect(amountInToBin).toBe(999717n)
+    expect(amountOutOfBin).toBe(199222843n)
     expect(volatilityAccumulated).toBe(0)
-    expect(feesTotal).toBe(100000000000n)
+    expect(feesTotal).toBe(283n)
   })
 })
