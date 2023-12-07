496320d8842b36a8d4cd43c95c865f2f8df4a426
jimmylies <tybernox@gmail.com>
2023-12-07 11:15:22 +0100

    chroe: update router vault address

--- src/constants/v2Addrs.ts
+++ src/constants/v2Addrs.ts
@@ -44,7 +44,7 @@ export const LIMIT_ORDER_MANAGER_ADDRESS: { [chainId in ChainId]: string } = {
 
 export const VAULT_MANAGER_ADDRESS: { [chainId in ChainId]: string } = {
   [ChainId.DUSANET]: '',
-  [ChainId.BUILDNET]: 'AS1fQLtwB5o3SMMKAdaTArBAiUhkzPk76Cbjgb9yWGD7JAuLLLex',
+  [ChainId.BUILDNET]: 'AS125znAYtVCJ2Qd5up1x7ffnDzuMEzMCYgyjJ82uc65FaeBFVPAn',
   [ChainId.TESTNET]: '',
   [ChainId.MAINNET]: ''
 }
