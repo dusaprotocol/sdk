4810f5b2bac44ff9dd8d783e9a1894e29c42bf6e
Pierre-Louis <93529605+plouis01@users.noreply.github.com>
2024-05-30 12:09:03 +0200

    feat: multisig sdk (#6)

--- src/types/periphery.ts
+++ src/types/periphery.ts
@@ -64,3 +64,50 @@ export interface StartDCAParameters {
   tokenPath: Address[]
   startIn: number
 }
+
+// export interface Transaction {
+//   to: string
+//   method: string
+//   value: bigint
+//   data: Uint8Array
+//   timestamp: bigint
+//   executed: boolean
+// }
+
+export class Transaction implements ISerializable<Transaction> {
+  constructor(
+    public to: string = '',
+    public method: string = '',
+    public value: bigint = 0n,
+    public data: Uint8Array = new Uint8Array(0),
+    public timestamp: bigint = 0n,
+    public executed: boolean = false
+  ) {}
+
+  serialize(): Uint8Array {
+    const args = new Args()
+    args.addString(this.to)
+    args.addString(this.method)
+    args.addU64(this.value)
+    args.addUint8Array(this.data)
+    args.addU64(this.timestamp)
+    args.addBool(this.executed)
+    return Uint8Array.from(args.serialize())
+  }
+
+  deserialize(data: Uint8Array, offset = 0): IDeserializedResult<Transaction> {
+    const args = new Args(data, offset)
+
+    this.to = args.nextString()
+    this.method = args.nextString()
+    this.value = args.nextU64()
+    this.data = args.nextUint8Array()
+    this.timestamp = args.nextU64()
+    this.executed = args.nextBool()
+
+    return {
+      instance: this,
+      offset: args.getOffset()
+    }
+  }
+}
