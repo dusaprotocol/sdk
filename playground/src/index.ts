e02393f42e68950f398e5b3db14e9f4e68c6684c
Nils <nipudos-01@yahoo.com>
2024-06-03 11:05:48 +0200

    ops: test before publishing

--- playground/src/index.ts
+++ playground/src/index.ts
@@ -2,14 +2,14 @@
 // import { swapAmountOut } from './swapAmountOut'
 // import { pricesAndBinsRelations } from './pricesAndBinsRelations'
 // import { getLBPairsAndActiveIds } from './getLBPairsAndActiveIds'
-import { addLiquidity } from './addLiquidity'
+import { pricesAndBinsRelations } from './pricesAndBinsRelations'
 // import { removeLiquidity } from './removeLiquidity'
 
 import dotenv from 'dotenv'
 dotenv.config() // loads env variables
 
 const main = async () => {
-  await addLiquidity()
+  await pricesAndBinsRelations()
 }
 
 main()
